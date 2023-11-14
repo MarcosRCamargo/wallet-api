c0fc5b1b2b9fcd44bfb3a165c2e860f5466c189a
Marcos Camargo <marcos.marrize@gmail.com>
2023-11-13 22:59:36 -0300

    Sequelize com migration e seed de Usuario

---
+++ src/app/seeders/20231114010924-demo-user.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'Usuário Exemplo',
+        email: 'usuario@example.com',
+        birthdate: new Date('1990-01-01'),
+        password: 'senha123',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
